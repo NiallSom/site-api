b200e60e68acff8fbffdcd576b83926b93e9b873
niall.somers <niall.somers@viotas.com>
2024-10-24 17:03:52 +0100

    feat(2): added specific get request and post request to add to the db

--- index.js
+++ index.js
@@ -12,11 +12,22 @@ async function main() {
         password: 'password',
         port: 3306
     });
-    
 
     app.get('/api/v1/sites', async (req, res) => {
         const [sites] = await pool.query("SELECT * FROM site_api_database.sites");
         res.send(sites);
+    })
+
+    app.get('/api/v1/sites/site', async (req, res) => {
+      const site = req.query.site;
+      const [site_specific] = await pool.query(`SELECT * FROM site_api_database.sites WHERE site_name='${site}'`);
+      res.send(site_specific);
+    })
+
+    app.post('/api/v1/sites/addSite', async (req, res) => {
+      const site_name = req.query.site_name;
+      const power = req.query.power;
+      await pool.query(`INSERT INTO site_api_database.sites (site_name, power) VALUES ('${site_name}', ${power});`)
     })
     app.listen(port, () => {
         console.log(`Listening on port ${port}`);
