6da3912ee42022be67b51f5a011ed2d6bcbcc182
niall.somers <niall.somers@viotas.com>
2024-10-25 11:07:59 +0100

    added the post, fixed formatting and added error catching

--- index.js
+++ index.js
@@ -20,14 +20,35 @@ async function main() {
 
     app.get('/api/v1/sites/site', async (req, res) => {
       const site = req.query.site;
-      const [site_specific] = await pool.query(`SELECT * FROM site_api_database.sites WHERE site_name='${site}'`);
+      if (!site) {
+        return res.status(400).json({
+            error: 'Missing required parameters: site_name is required'
+        });
+        }
+      const [site_specific] = await pool.query(`SELECT * FROM site_api_database.sites WHERE site_name=?`, site_name);
       res.send(site_specific);
     })
 
-    app.post('/api/v1/sites/addSite', async (req, res) => {
+    app.post('/api/v1/sites/add', async (req, res) => {
       const site_name = req.query.site_name;
       const power = req.query.power;
-      await pool.query(`INSERT INTO site_api_database.sites (site_name, power) VALUES ('${site_name}', ${power});`)
+      if (!site_name || !power) {
+        return res.status(400).json({
+            error: 'Missing required parameters: site_name and power are required'
+        });
+        }
+      await pool.query(`INSERT INTO site_api_database.sites (site_name, power) VALUES (?, ?);`, [site_name, power]);
+    })
+
+    app.post('/api/v1/sites/update', async (req, res) => {
+        const site_name = req.query.site_name;
+        const power = req.query.power;
+        if (!site_name || !power) {
+            return res.status(400).json({
+                error: 'Missing required parameters: site_name and power are required'
+            });
+        }
+        await pool.query(`UPDATE site_api_database.sites SET power=? WHERE site_name=?;`,[site_name, power]);
     })
     app.listen(port, () => {
         console.log(`Listening on port ${port}`);
